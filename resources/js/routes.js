ef23292260fcfe774376572a8edf8cafa02d7732
Ajay Kumar Yadav <ajaykucse11@outlook.com>
2021-06-19 16:02:47 +0545

    Create CRUD for Category Module

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -16,6 +16,11 @@ let storesupplier = require('./components/supplier/create.vue').default;
 let supplier = require('./components/supplier/index.vue').default;
 let editsupplier = require('./components/supplier/edit.vue').default;
 
+// Category components 
+let storecategory = require('./components/category/create.vue').default;
+let category = require('./components/category/index.vue').default;
+let editcategory = require('./components/category/edit.vue').default;
+
 
 export const routes = [
 	{ path: '/', component: login, name:'/'},
@@ -32,5 +37,10 @@ export const routes = [
 	//Supplier Routes
 	{ path: '/store-supplier', component: storesupplier, name:'store-supplier'},
 	{ path: '/supplier', component: supplier, name:'supplier'},
-	{ path: '/edit-supplier/:id', component: editsupplier, name:'edit-supplier'}
+	{ path: '/edit-supplier/:id', component: editsupplier, name:'edit-supplier'},
+
+	//Category Routes
+	{ path: '/store-category', component: storecategory, name:'store-category'},
+	{ path: '/category', component: category, name:'category'},
+	{ path: '/edit-category/:id', component: editcategory, name:'edit-category'}
 ]
